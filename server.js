1b5e2d3ee6f1acc39c667251cb280a1949acaf0b
harshgupta9321 <112786943+harshgupta9321@users.noreply.github.com>
2023-10-29 06:29:15 +0530

    firstcommit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var mongoose = require('mongoose');
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
 
-mongoose.connect('mongodb+srv://<DB_USER_NAME>:<DB_PASSWORD>@cluster0-vatbg.mongodb.net/registrationFormHeruko?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://harshgupta18022003:hellomongo@cluster0.7yw5j5y.mongodb.net/first?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }, (err) => {
